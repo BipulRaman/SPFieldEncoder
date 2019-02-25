b455d7c1bc664ea13a33bf58412651d6fb95f695
Bipul Raman <bipulraman@gmail.com>
2019-02-25 22:06:18 +0530

    Codes added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# SPFieldEncoder
-SP Field Name Encoder
+# SP Field Name Ecoder Decoder
+
+A web-based utility to encode or decode SharePoint field name.
+
+Link: https://spencode.bipul.in
+
+# License
+All files within this repo are released under the [MIT (OSI) License]( https://en.wikipedia.org/wiki/MIT_License) as per the [LICENSE file](https://github.com/JNVSAlumni/JNVSAlumni.github.io/blob/master/LICENSE) stored in the root of this repo. 
+
+Third party libraries and license are summarised below:
+
+|Library|Version|Author|Licence|
+|---|---|---|---|
+|Bootstrap|4.3.1|Twitter, Inc. & The Bootstrap Authors |[MIT]( https://en.wikipedia.org/wiki/MIT_License)|
---
+++ app.js
@@ -0,0 +1,46 @@
+function Convert() {
+    var convertType = document.querySelector('input[name="convertType"]:checked').value;
+    if (convertType == "Encode") {
+        document.getElementById('txtOutput').value = SPEncode(document.getElementById('txtInput').value);
+    }
+    else if (convertType == "Decode") {
+        document.getElementById('txtOutput').value = SPDecode(document.getElementById('txtInput').value);
+    }
+}
+
+function CopyOutput() {
+    document.getElementById("txtOutput").select();
+    document.execCommand("copy");
+}
+
+function ClearForm(){
+    document.getElementById("convert").reset();
+}
+
+// Encode
+function SPEncode(toEncode) {
+    var charToEncode = toEncode.split('');
+    var encodedString = "";
+
+    for (i = 0; i < charToEncode.length; i++) {
+        encodedChar = escape(charToEncode[i]).toLowerCase();
+
+        if (encodedChar.length == 3) {
+            encodedString += encodedChar.replace("%", "_x00") + "_";
+        }
+        else if (encodedChar.length == 5) {
+            encodedString += encodedChar.replace("%u", "_x") + "_";
+        }
+        else {
+            encodedString += encodedChar;
+        }
+    }
+    return encodedString;
+}
+
+// Decode
+function SPDecode(toDecode) {
+    var decodedString = toDecode.replace("_x", "%u").replace("_", "");
+
+    return unescape(decodedString);
+}
